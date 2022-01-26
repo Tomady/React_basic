144d0bd63c59461896c7589842bef5d77462b4d0
Tomady <87797173+Tomady@users.noreply.github.com>
2022-01-26 19:46:48 +0900

    section 7

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const {User} = require('./models/User');
+
+//application/x-www-form-urlencoded 분석
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json 분석
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://dbUser:1234@movieapp.skjcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 .then(() => {
@@ -11,4 +19,17 @@ mongoose.connect('mongodb+srv://dbUser:1234@movieapp.skjcz.mongodb.net/myFirstDa
 });
 
 app.get('/', (req, res) => res.send('Hello World~~ 안녕하세요 ~'));
+app.post('/register', (req, res) => {
+    // 회원 가입할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    console.log('register');
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err});
+        return res.status(200).json({
+            success: true
+        })
+    })
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
